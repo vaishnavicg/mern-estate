06e9b382c74a2407e8ae6e8509b6aac03b3f21ed
vaishnavicg <01fe21bcs271@kletech.ac.in>
2023-12-09 20:55:46 +0530

    pages

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom' ;
+import Home from './pages/home'
+import About from './pages/about'
+import Profile from './pages/profile'
+import Signin from './pages/signin'
+import Signup from './pages/signup'
+
 
 export default function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/profile" element={<Profile/>}></Route>
+    <Route path="/signin" element={<Signin/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+
+  </Routes>
+  </BrowserRouter>
+    
+  
 }
